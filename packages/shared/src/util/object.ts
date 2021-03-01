199c25958a19bc7efb0da93f4f77ef57c66f87fa
imere <838914662@qq.com>
2021-03-01 17:15:41 +0800

    refactor(shared): add function
    - assign
    - simpleMerge

--- packages/shared/src/util/object.ts
+++ packages/shared/src/util/object.ts
@@ -60,6 +60,41 @@ export function omit<T, K extends keyof T>(obj: T, keys: K[]): Omit<T, K> {
 }
 
 /**
+ * assign value to object by path
+ * @example
+ * const target = {}
+ * assign(target, 'a.b.c', 5)
+ * console.log(target) // { a: { b: { c: 5 } } }
+ * @export
+ * @param {(Record<string | number, unknown>)} target
+ * @param {string} path
+ * @param {unknown} source
+ * @returns {void}
+ */
+export function assign(target: Record<string | number, unknown>, path: string, source: unknown): void {
+  if (isNullOrUndef(target)) return;
+
+  const namespaces = path.split('.');
+  const targetName = namespaces.pop();
+
+  if (isNullOrUndef(targetName)) throw new TypeError(`assign: got wrong target ${targetName} from path ${path}`);
+
+  if (!namespaces.length) {
+    target[targetName as string] = source;
+    return;
+  }
+
+  let result = target;
+
+  namespaces.forEach(namespace => {
+    if (!result[namespace]) result[namespace] = {};
+    result = result[namespace] as typeof target;
+  });
+
+  result[targetName] = source;
+}
+
+/**
  * Make all properties optional for typing
  *
  * @export
@@ -69,4 +104,41 @@ export function omit<T, K extends keyof T>(obj: T, keys: K[]): Omit<T, K> {
  */
 export function partialType<T>(obj: T): Partial<T> {
   return obj;
+}
+
+type O = Record<string, unknown>
+/**
+ * Simple deep merge, only works for object who's
+ * values are `primitive`、`array` and `pure object`
+ * @export
+ * @param {O} target
+ * @param {O} source
+ * @param {boolean} [concat=false] whether concat array, default false
+ * @returns {O}
+ */
+export function simpleMerge(target: O, source: O, concat = false): O {
+  if (!source) return target;
+  if (!target) return source;
+
+  const res = {
+    ...target,
+  };
+
+  let val;
+  Object.keys(source).forEach(key => {
+    val = source[key];
+    if (Array.isArray(val)) {
+      if (concat && (res[key] as [])?.push) {
+        (res[key] as unknown[]).push(val);
+      } else {
+        res[key] = val;
+      }
+    } else if (typeof val === 'object') {
+      res[key] = simpleMerge(target[key] as O, val as O);
+    } else {
+      res[key] = val;
+    }
+  });
+
+  return res;
 }
