281fdb74ec233025a91777f347b9f7208eeee0ed
imere <838914662@qq.com>
2021-03-01 17:24:10 +0800

    refactor(client): use analytics

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,5 +1,8 @@
+import { runAnalytics } from '@packages/analytics-front/index';
 import { render } from 'react-dom';
 import { App } from './App';
 import './styles/global.css';
+
+runAnalytics();
 
 render(<App />, document.getElementById('__root'));
